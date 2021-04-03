8b5936b23c1b86187c57ae12910a6fe86d859b30
Alkathos <f.zaldivar@live.com>
2021-04-03 16:46:48 -0400

    Moved question to a separate js file and added code for getQuestion and questionClick

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+/*----------questions---------*/
+var jsQuestions = [ 
+    {
+        text: "What data type is true and false?",
+        choices: ["Boolean", "Variable", "Operator", "String"],
+        answer: "Boolean"
+    },
+    {
+        text: "What data type is any random text?",
+        choices: ["Boolean", "Variable", "Operator", "String"],
+        answer: "String"
+    },
+    {
+        text: "What data type are the +, -, = and ===?",
+        choices: ["Boolean", "Variable", "Operator", "String"],
+        answer: "Operator"
+    },
+    {
+        text: "What data type is true and false?",
+        choices: ["Boolean", "Variable", "Operator", "String"],
+        answer: "Boolean"
+    },
+    {
+        text: "What data type is true and false?",
+        choices: ["Boolean", "Variable", "Operator", "String"],
+        answer: "Boolean"
+    }
+    ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,17 @@ var startButton = document.getElementById("start-btn");
 var instruction = document.getElementById("begininginstruction");
 var questionElement = document.getElementById("question");
 var answerButtonsElement = document.getElementById("answergrid");
+var quizTimer = document.getElementById("timer");
+var startButton = document.getElementById("start-btn");
 
 /*----------global variables---------*/
-var currentQuestionIndex;
+var currentQuestionIndex = 0;
 var timer;
 var count;
+var time = jsQuestions.length * 15;
 
-/*----------questions---------*/
-var jsQuestions = [ {
-    question: "What data type is true and false?",
-    choices: ["Boolean", "Variable", "Operator", "String"],
-    answer: "Boolean"
-}];
-
-
+//When I click the button that has the "start" id associated with it, it will wait for a click and begin the countdown.
+startButton.addEventListener("click", startQuiz);
   
 //this functions tells the browser what happens when the timer reaches 0
 function quizOver () {
@@ -26,29 +23,84 @@ function quizOver () {
 }
 
 
-//this begins the countdown by grabbing the variables timer and count. Sets the count to 80 and decreases the count by the timer interval
+//this begins the countdown by grabbing the variables timer and count. Sets the count to 80 and decreases the count by 1 second
 function startTimer () {
-    //initialize the count at 80
     count = 80;
-    //this will tell the browser to start reducing the timer by 1sec(1000ms)
     timer = setInterval(function() {
         count--;
-        if(count===0) {
+        if(count<0) {
             quizOver();
             clearInterval(timer);
         } else {
-            document.getElementById("timer").textContent=count;
+            quizTimer.textContent=count;
         }
     }, 1000);
 }
 
-function showNextQuestion(question) {
-   
-}
 
-function renderQuestion(question) {
-    questionElement.innerText = jsQuestions.question;
-}
+function getQuestion() {
+    // get current question object from array
+    var currentQuestion = jsQuestions[currentQuestionIndex];
+  
+    // update title with current question  
+    questionElement.textContent = currentQuestion.text;
+  
+    // clear out any old question choices
+    answerButtonsElement.innerHTML = "";
+  
+    // loop over choices
+    currentQuestion.text.forEach(function(text, i) {
+      // create new button for each choice
+      var choiceNode = document.createElement("button");
+      choiceNode.setAttribute("class", "choice");
+      choiceNode.setAttribute("value", text);
+  
+      choiceNode.textContent = i + 1 + ". " + choice;
+  
+      // attach click event listener to each choice
+      choiceNode.onclick = questionClick;
+  
+      // display on the page
+      answerButtonsElement.appendChild(choiceNode);
+    });
+  }
+
+  function questionClick() {
+    // check if user guessed wrong
+    if (this.value !== jsQuestions[currentQuestionIndex].answer) {
+      // penalize time
+      time -= 15;
+  
+      if (time < 0) {
+        time = 0;
+      }
+  
+      // display new time on page
+      quizTimer.textContent = time;
+  
+  
+      feedbackEl.textContent = "Wrong!";
+    } else {
+
+      feedbackEl.textContent = "Correct!";
+    }
+  
+    // flash right/wrong feedback on page for half a second
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+  
+    // move to next question
+    currentQuestionIndex++;
+  
+    // check if we've run out of questions
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+  }
 
 //function to begin the game which references the startTimer function.
 function startQuiz() {
@@ -56,10 +108,7 @@ function startQuiz() {
     startButton.style.display = "none";
     instruction.style.display = "none";
     quizContainer.classList.remove("hide");
-    currentQuestionIndex = 0;
     answerContainer.classList.remove("hide");
-    renderQuestion();
+    getQuestion();
 }
 
-//When I click the button that has the "start" id associated with it, it will wait for a click and begin the countdown.
-document.getElementById("start-btn").addEventListener("click", startQuiz);
